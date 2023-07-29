6279bb95f1315b4c6a447aed4863e2a4c8c1b6a4
Aman <amanmalik3105@gmail.com>
2023-07-29 18:00:57 +0530

    Button Components

---
+++ conatactUs/src/componenets/Button/Button.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {MdMessage} from "react-icons/md"
+import styles from "./Button.module.css"
+const Button = (props) => {
+  return (
+    <button className={styles.primary_btn}>
+        {props.icon}
+        {props.text}
+        </button>
+  )
+}
+
+export default Button
--- conatactUs/src/componenets/ContactForm/ContactForm.jsx
+++ conatactUs/src/componenets/ContactForm/ContactForm.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
 import styles from "./ContactForm.module.css"
+import Button from '../Button/Button'
+import {MdMessage} from "react-icons/md"
+import {FaPhoneAlt} from 'react-icons/fa'
 const ContactForm = () => {
   return (
-    <div>ContactForm</div>
+    <section className={styles.container}>
+        <div className={styles.contact_form}>
+            <div className={styles.top_button}>
+            <Button
+            text ="VIA SUPPORT CHAT"
+            icon ={<MdMessage fontSize="24" />}
+            />
+            <Button
+            text ="VIA CALL"
+            icon ={<FaPhoneAlt fontSize="24"/>}
+            />
+            </div>
+        </div>
+        <div className='styles.contact_image'></div>
+    </section>
   )
 }
 
