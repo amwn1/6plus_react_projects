bfdd9fbca0819dfb81759de757361bb049220a5b
Aman <amanmalik3105@gmail.com>
2023-08-19 22:17:37 +0530

    Search bar done

--- crudApp/src/App.jsx
+++ crudApp/src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Navbar from './components/Navbar'
-
+import {BiSearchAlt} from 'react-icons/bi'
 const App = () => {
   return (
-    <div className=' max-w-[370px] mx-auto '>
+    <div className=' max-w-[370px] mx-auto px-4'>
      <Navbar /> 
+     <div className='flex relative items-center'>
+     <BiSearchAlt className=' ml-2 text-white absolute text-3xl position '/>
+      <input type="text" className=' pl-10 text-white flex-grow bg-transparent border-white h-10 rounded-md border' />
+     </div>
     </div>
   )
 }
--- crudApp/src/components/Navbar.jsx
+++ crudApp/src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Navbar = () => {
-  return <div className="gap-2 text-xl font-medium m-4 h-[60px] bg-white rounded-lg flex items-center justify-center">
+  return <div className="gap-2 text-xl font-medium my-4 h-[60px] bg-white rounded-lg flex items-center justify-center">
    
         <img src="/logofb.png" alt="logo" />
         <h1>Contact App</h1>
