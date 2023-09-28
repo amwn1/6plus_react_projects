cda1cf77599db0c7a520494deb75d4f487142d42
Aman <amanmalik3105@gmail.com>
2023-09-28 22:40:06 +0530

    Navbar static is done

--- responsive/src/components/Navbar/Navbar.jsx
+++ responsive/src/components/Navbar/Navbar.jsx
@@ -131,4 +131,17 @@ border-radius: 50px;
 background: #01bf71;
 white-space: nowrap;
 padding: 10px 22px;
+color: #010606;
+font-size: 1rem;
+outline:none;
+border:none;
+cursor: pointer;
+transition: all 0.2s ease-in-out;
+text-decoration: none;
+
+&:hover{
+  transition: all 0.2s ease-in-out;
+  background: #fff;
+  color: #010606;
+  }
 `
