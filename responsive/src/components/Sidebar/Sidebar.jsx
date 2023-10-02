11afa8c3cc4f8baa2e08accc4c26ae6a309681dc
Aman <amanmalik3105@gmail.com>
2023-10-02 23:01:23 +0530

    Sidebar styling

--- responsive/src/components/Sidebar/Sidebar.jsx
+++ responsive/src/components/Sidebar/Sidebar.jsx
@@ -1,12 +1,33 @@
 import React from 'react'
 import styled from 'styled-components'
 import {FaTimes} from 'react-icons/fa'
+import { Link as LinkR  } from 'react-router-dom';
+import { Link as LinkS  } from 'react-scroll';
 const Sidebar = () => {
   return (
     <Sidebarcontainer>
         <Icon>
             <CloseIcon/>
         </Icon>
+        <SideBarWrapper>
+            <SidebarMenu>
+                <SidebarLink to = "about">
+                    About
+                </SidebarLink>
+                <SidebarLink to = "discover">
+                    Discover
+                </SidebarLink>
+                <SidebarLink to = "services">
+                    Services
+                </SidebarLink>
+                <SidebarLink to = "signup">
+                    Signup
+                </SidebarLink>
+            </SidebarMenu>
+            <SideBtnWrap>
+                <SidebarRoute>Sign In</SidebarRoute>
+            </SideBtnWrap>
+        </SideBarWrapper>
     </Sidebarcontainer>
   )
 }
@@ -42,3 +63,50 @@ cursor:pointer;
 outline:none;
 
 `
+
+const SideBarWrapper = styled.div`
+    color:#fff;
+`
+const SidebarMenu = styled.div`
+    
+`
+const SidebarLink = styled(LinkS)`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size:1.5rem;
+    text-decoration: none;
+    list-style: none;
+    transition: 0.2s ease-in-out;
+    color: #fff;
+    cursor:pointer;
+
+    &:hover{
+        color:#01bf71;
+        transition: 0.2s ease-in-out;
+
+    }
+`
+const SideBtnWrap= styled.div`
+display: flex;
+justify-content: center;
+
+`
+const SidebarRoute = styled(LinkR)`
+border-radius: 50px;
+background: #01bf71;
+white-space: nowrap;
+padding:16px 64px;
+color:#010606;
+font-size: 16px;
+outline:none;
+border:none;
+transition: all 0.2 ease-in-out;
+text-decoration: none;
+
+&:hover{
+    transition: all 0.2 ease-in-out;
+    background: #fff;
+    color:#010606;
+}
+` 
