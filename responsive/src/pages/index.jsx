8d1147fc18770e3bf33885c43a266792f819570f
Aman <amanmalik3105@gmail.com>
2023-10-09 23:34:58 +0530

    Index page

---
+++ responsive/src/pages/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Sidebar from '../components/Sidebar/Sidebar'
+import Navbar from '../components/Navbar/Navbar'
+const index = () => {
+  return (
+    <>
+    <Sidebar />
+    <Navbar />
+    </>
+  )
+}
+
+export default index
