c8bd878326b5cfe8b8aba41b3d354316922089ae
Aman <amanmalik3105@gmail.com>
2023-10-10 21:52:42 +0530

    cleaning code

--- responsive/src/App.jsx
+++ responsive/src/App.jsx
@@ -1,9 +1,9 @@
-import { useState } from 'react'
+
 import styled from 'styled-components'
 import './App.css'
-import Navbar from './components/Navbar/Navbar'
+
 import {BrowserRouter as Router} from 'react-router-dom'
-import Sidebar from './components/Sidebar/Sidebar'
+
 import Home from './pages'
 
 function App() {
@@ -11,7 +11,7 @@ function App() {
 
   return (
       <Router>
-        <Home/>
+        <Home/> 
        </Router>
    
   )
